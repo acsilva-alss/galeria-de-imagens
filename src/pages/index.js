6dc365de8c059937da9a8f8e73769cad6399e756
Alisson Chagas Silva <alisson.cs@hotmail.com>
2021-03-09 19:56:55 -0300

    Add postcss and tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import { graphql, useStaticQuery} from 'gatsby'
 import Image from 'gatsby-image'
 import { v4 as uuidv4 } from 'uuid';
 
+import '../styles/styles.css'
+
 const Index = () =>{
     const images = useStaticQuery(graphql`
     query MyQuery {
@@ -10,10 +12,11 @@ const Index = () =>{
           edges {
             node {
               childImageSharp {
-                fluid {
+                fluid(maxHeight: 240, maxWidth: 480) {
                     ...GatsbyImageSharpFluid
                 }
               }
+              relativePath
             }
           }
         }
@@ -22,10 +25,18 @@ const Index = () =>{
     `)
     return(
         <div>
-        <h1>Galeria de imagens</h1>
+        <h1 className='text-6xl shadow'>Galeria de imagens</h1>
+        <div className='grid max-w-4xl grid-cols-2 gap-4 mx-auto p-6'>
         {images.allFile.edges.map(image => {
-            return <Image key={uuidv4()} fluid={image.node.childImageSharp.fluid}/>
+          console.log(image)
+            return (
+              <div className='shadow rounded'>
+                <Image className='rounded-t' key={uuidv4()} fluid={image.node.childImageSharp.fluid}/>
+                <p className='p-6'>{image.node.relativePath}</p>
+              </div>
+            )
         })}
+        </div>
         </div>
     )
 }
